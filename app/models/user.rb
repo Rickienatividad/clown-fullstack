75fa86586c03e7d3d357e64bacb991b7f42adc2e
Rickie Natividad <rickienatividad85@gmail.com>
2022-09-27 14:14:52 -0400

    adds associations between users, clowns, bookings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :bookings
+  has_many :clowns, through: :bookings
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,50 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-# user = User.create(
-#   email: "bob@test.com",
-#   password: "password",
-#   password_confirmation: "password",
-# )
+user = User.create(
+  email: "bob@test.com",
+  password: "password",
+  password_confirmation: "password",
+)
 
-# user = User.create(
-#   email: "jan@test.com",
-#   password: "password",
-#   password_confirmation: "password",
-# )
+user = User.create(
+  email: "jan@test.com",
+  password: "password",
+  password_confirmation: "password",
+)
 
-# user = User.create(
-#   email: "fred@test.com",
-#   password: "password",
-#   password_confirmation: "password",
-# )
+user = User.create(
+  email: "fred@test.com",
+  password: "password",
+  password_confirmation: "password",
+)
 
 #clowns
-# clown = Clown.create(
-#   name: "sadface sillypants",
-#   rate: 85,
-# )
+clown = Clown.create(
+  name: "sadface sillypants",
+  rate: 85,
+)
 
-# clown = Clown.create(
-#   name: "bobo bignose",
-#   rate: 90,
-# )
+clown = Clown.create(
+  name: "bobo bignose",
+  rate: 90,
+)
 
-# clown = Clown.create(
-#   name: "weepy",
-#   rate: 70,
-# )
+clown = Clown.create(
+  name: "weepy",
+  rate: 70,
+)
 
-# clown = Clown.create(
-#   name: "dumpy",
-#   rate: 75,
-# )
+clown = Clown.create(
+  name: "dumpy",
+  rate: 75,
+)
 
-# Bookings
+#Bookings
 booking = Booking.create(
   user_id: 1,
   clown_id: 1,
